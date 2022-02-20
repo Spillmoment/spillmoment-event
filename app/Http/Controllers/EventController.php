1fb40caf5a71b6e63f15e392cece3925625975a0
hafet17 <do.crazy192@gmail.com>
2022-02-20 12:18:24 +0700

    create query event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\EventCategory;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
     public function index()
     {
-        return view('pages.event.index');
+        $category = EventCategory::all();
+        $event = Event::all();
+        $status = Event::where('status', 'online')
+            ->orWhere('status', 'offline')
+            ->groupBy('status')
+            ->get();
+        $type = Event::where('type', 'paid')
+            ->orWhere('type', 'free')
+            ->groupBy('type')
+            ->get();
+
+        return view('pages.event.index', [
+            'category' => $category,
+            'events' => $event,
+            'status' => $status,
+            'type' => $type
+        ]);
     }
 
     public function detail()
