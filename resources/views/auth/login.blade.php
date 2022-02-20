165c79e11af3bff5a2dc558092da74a2983ace60
hafet17 <do.crazy192@gmail.com>
2022-02-20 09:30:00 +0700

    implement login spill

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,89 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.auth')
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('content')
+<div id="auth-login">
+    <div class="box-v1">
+        <div class="flex-box">
+            <div class="col">
+                <div class="col-body">
+                    <div class="auth-box">
+                        <div class="auth-icon-brand">
+                            <img src="{{ asset('img/logo-spillmoment.png') }}" alt="" />
+                        </div>
+                        <div class="auth-title">
+                            <h2>Login</h2>
+                            <p>Masuk dengan akun yang sudah terdaftar.</p>
+                        </div>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="auth-body">
+                                <p>Email atau nomor ponsel</p>
+                                <div class="auth-input-box">
+                                    <input type="text" name="" id="userphonemail" placeholder="" required />
+                                </div>
+                                <br />
+                                <p>Kata Sandi</p>
+                                <div class="auth-input-box">
+                                    <div class="input-box-password">
+                                        <input type="password" name="password" placeholder="" id="passwordInput"
+                                            required />
+                                        <label for="password-function" class="checkbox-pass">
+                                            <input type="checkbox" name="" id="password-function" />
+                                            <div class="icon-eye1 icon">
+                                                <i class="fas fa-eye-slash"></i>
+                                            </div>
+                                            <div class="icon-eye2 icon">
+                                                <i class="fas fa-eye"></i>
+                                            </div>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                        <div class="wrap-button">
+                            <br />
+                            @if (Route::has('password.request'))
+                            <a href="{{ route('password.request') }}">Lupa kata sandi?</a>
+                            @endif
+                            <button>Masuk</button>
+                            <br /><br />
+                        </div>
+                        <p id="hint">
+                            Apakah anda belum mendaftar?<a href="/register">
+                                <span style="margin-rigth: 5px"><b>Daftar Sekarang</b></span>
+                            </a>
+                        </p>
+                    </div>
+                </div>
             </div>
+            <div class="col"></div>
+        </div>
+    </div>
+</div>
+@endsection
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+@push('script')
+<script>
+    var user = document.querySelector("#userphonemail").value;
+    var pass = document.querySelector("#password").value;
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+    var showHidepass = document.querySelector("#password-function");
+    var iconEyeOne = document.querySelector(".icon-eye1");
+    var iconEyeTwo = document.querySelector(".icon-eye2");
+    var passInput = document.querySelector("#passwordInput");
+    showHidepass.addEventListener("click", function () {
+        if (showHidepass.checked == true) {
+            iconEyeOne.style.display = "none";
+            iconEyeTwo.style.display = "block";
+            passInput.type = "text";
+        } else {
+            iconEyeOne.style.display = "block";
+            iconEyeTwo.style.display = "none";
+            passInput.type = "password";
+        }
+    });
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</script>
+@endpush
