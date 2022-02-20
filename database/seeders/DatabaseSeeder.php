5f96f6d04ab157117659a90de15106e55d84547c
Deddy Gunawan <deddygunawan98@gmail.com>
2022-02-20 12:34:39 +0700

    lupa koma guyss wkwkw

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            AdminSeeder::class
+            AdminSeeder::class,
             EventCategorySeeder::class,
             SpeakerSeeder::class,
             EventSeeder::class,
