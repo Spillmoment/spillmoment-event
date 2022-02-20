9d6701b8a1c2d84858f70ee5c28b0bf18b3d7595
hafet17 <do.crazy192@gmail.com>
2022-02-20 09:30:08 +0700

    implement register spill

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,123 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.auth')
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+@section('content')
+<div id="auth-register">
+    <div class="box-v1">
+        <div class="flex-box">
+            <div class="col">
+                <div class="col-body">
+                    <div class="auth-box">
+                        <div class="auth-icon-brand">
+                            <img src="{{ asset('img/logo-spillmoment.png') }}" alt="" />
+                        </div>
+                        <div class="auth-title">
+                            <h2>Daftar</h2>
+                            <p>Daftarkan akun sebagai member dari kami.</p>
+                        </div>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+                            <div class="auth-body">
+                                <p>Masukan Nama</p>
+                                <div class="auth-input-box">
+                                    <input type="text" name="" id="nama" placeholder="nama kamu?" required />
+                                </div>
+                                <br />
+                                <p>Alamat Email</p>
+                                <div class="auth-input-box">
+                                    <input type="email" name="" id="email" placeholder="Masukan alamat email yang ada"
+                                        required />
+                                </div>
+                                <br />
+                                <p>Nomor Ponsel (opsional)</p>
+                                <div class="auth-input-box">
+                                    <input type="number" name="" id="phone" placeholder="contoh: +6208974xxxx"
+                                        required />
+                                </div>
+                                <br />
+                                <p>Jenis kelamin</p>
+                                <div class="auth-input-box">
+                                    <div class="flex-input">
+                                        <select name="" id="select-input gender">
+                                            <option value="Pria">Pria</option>
+                                            <option value="Wanita">Wanita</option>
+                                        </select>
+                                        <button>
+                                            <fa :icon="['fas', 'caret-down']" />
+                                        </button>
+                                    </div>
+                                </div>
+                                <br />
+                                <p>Tanggal lahir</p>
+                                <div class="auth-input-box">
+                                    <input type="date" name="" id="born_date" placeholder="" required />
+                                </div>
+                                <br />
+                                <p>Buat Kata sandi</p>
+                                <div class="auth-input-box">
+                                    <div class="input-box-password">
+                                        <input type="password" name="katasandi" placeholder=""
+                                            v-on:keyup="isGood(this.value)" id="passwordInput" required />
+                                        <label for="password-function" class="checkbox-pass">
+                                            <input type="checkbox" name="" id="password-function" />
+                                            <div class="icon-eye1 icon">
+                                                <fa :icon="['fas', 'eye-slash']" />
+                                            </div>
+                                            <div class="icon-eye2 icon">
+                                                <fa :icon="['fas', 'eye']" />
+                                            </div>
+                                        </label>
+                                    </div>
+                                </div>
+                                <small id="password-text"></small>
+                            </div>
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+
+                        </form>
+
+                        <div class="wrap-button">
+                            <br />
+                            <a href="/forgot-password">Lupa kata sandi?</a>
+                            <button @click="reg">Lanjutkan</button>
+                            <br /><br />
+                        </div>
+                        <p id="hint">
+                            Sudah punya akun terdaftar?<router-link to="/login"><b>Masuk!</b></router-link>
+                        </p>
+                    </div>
+                </div>
             </div>
+        </div>
+    </div>
+</div>
+@endsection
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+@push('script')
+<script>
+    var name = document.querySelector("#nama").value;
+    var email = document.querySelector("#email").value;
+    var phone = document.querySelector("#phone").value;
+    var password = document.querySelector("#passwordInput").value;
+    var gender = document.querySelector("#gender").value;
+    var borndate = document.querySelector("#born_date").value;
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+    var showHidepass = document.querySelector("#password-function");
+    var iconEyeOne = document.querySelector(".icon-eye1");
+    var iconEyeTwo = document.querySelector(".icon-eye2");
+    var passInput = document.querySelector("#passwordInput");
+    showHidepass.addEventListener("click", function () {
+        if (showHidepass.checked == true) {
+            iconEyeOne.style.display = "none";
+            iconEyeTwo.style.display = "block";
+            passInput.type = "text";
+        } else {
+            iconEyeOne.style.display = "block";
+            iconEyeTwo.style.display = "none";
+            passInput.type = "password";
+        }
+    });
+    },
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</script>
+@endpush
