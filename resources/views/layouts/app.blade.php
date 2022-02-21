91dea829dd1ef3594778ed8d75b50693ec1a300c
hafet17 <do.crazy192@gmail.com>
2022-02-22 00:20:06 +0700

    refactor parent app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,29 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    {{-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"> --}}
-    <link rel="stylesheet" href="/css/main.css">
-    <link rel="stylesheet" href="/css/all.css">
-    <link rel="stylesheet" href="/css/animate.css">
-    <link rel="stylesheet" href="/vendor/owl-carousel/assets/owl.carousel.min.css">
-    <link rel="shortcut icon" href="https://cdn.spillmoment.id/img/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="/vendor/owl-carousel/assets/owl.theme.default.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-        integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @include('layouts.style')
+    @stack('styles')
 </head>
 
 <body>
     <div id="app">
+
         @include('components.header')
 
         @yield('content')
 
         @include('components.footer')
+
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="/vendor/owl-carousel/owl.carousel.min.js"></script>
+    @include('layouts.script')
     @stack('script')
 </body>
 
