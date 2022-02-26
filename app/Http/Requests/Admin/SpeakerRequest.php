6b4727b7135b30317f5c0d4fe2d62d82939ff17a
hafet17 <do.crazy192@gmail.com>
2022-02-26 08:28:51 +0700

    add: speaker request

--- app/Http/Requests/Admin/SpeakerRequest.php
+++ app/Http/Requests/Admin/SpeakerRequest.php
@@ -6,25 +6,31 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class SpeakerRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
+        if (request()->isMethod('post')) {
+            $email = 'required|email|unique:speakers';
+            $phone = 'required|unique:speakers';
+            $photo = 'required|image|mimes:jpeg,jpg,png,bmp';
+        } elseif (request()->isMethod('put')) {
+            $email = 'required|email|unique:speakers,email,' . $this->speaker->id;
+            $phone = 'required|unique:speakers,phone,' . $this->speaker->id;
+            $photo = 'sometimes|nullable|image|mimes:jpeg,jpg,png,bmp';
+        }
+
         return [
-            //
+            'name' => 'required|string',
+            'email' => $email,
+            'phone' => $phone,
+            'photo' => $photo,
+            'address' => 'required',
+            'competence' => 'required',
+            'position' => 'required'
         ];
     }
 }
