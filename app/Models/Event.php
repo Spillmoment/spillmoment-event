e6f1fd8cd5f5fd72b2badbc738a01eff3fcc650d
Deddy Gunawan <deddygunawan98@gmail.com>
2022-02-28 20:19:50 +0700

    add fillable event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -9,6 +9,9 @@ class Event extends Model
 {
     use HasFactory;
 
+	 protected $fillable = [
+		'id', 'event_category_id', 'speaker_id', 'title', 'slug', 'body', 'photo', 'price', 'link', 'partner', 'quota', 'type', 'status', 'place'
+	];
     protected $dates = ['start_time', 'event_date'];
 
     public function category()
