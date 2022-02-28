dc6dd070c2e51f96b7c0d2111e24da31f8faf739
hafet17 <do.crazy192@gmail.com>
2022-02-28 09:47:31 +0700

    feat(event-register): added hasmany relationship in models

--- app/Models/EventRegister.php
+++ app/Models/EventRegister.php
@@ -10,4 +10,14 @@ class EventRegister extends Model
     use HasFactory;
 
     protected $fillable = ['event_id', 'user_id'];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class, 'event_id');
+    }
 }
