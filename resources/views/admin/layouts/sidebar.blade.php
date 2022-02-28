f80a3338bacd07575e71f19ceb75b3d3c08a32f2
hafet17 <do.crazy192@gmail.com>
2022-02-28 09:51:20 +0700

    fix(sidebar): update list sidenav

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -5,15 +5,20 @@
 
             <div class="d-flex align-items-center">
                 <div class="user-avatar lg-avatar mr-4">
-                    <img src="" class="card-img-top rounded-circle border-white" alt="Bonnie Green">
+                    <img @isset(Auth::user()->photo)
+                    src="{{ asset('uploads/users/' . Auth::user()->photo) }}"
+                    @else
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                    @endisset
+                    class="card-img-top rounded-circle border-white" alt="Bonnie Green">
                 </div>
                 <div class="d-block">
-                    <h2 class="h6">Hi, {{ auth()->user()->name ?? '' }}</h2>
-                    <a href="{{ route('login') }}" onclick="event.preventDefault();
+                    <h2 class="h6">Hi, {{ auth()->user()->name ?? 'Guest' }}</h2>
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
       document.getElementById('logout-form').submit();" class="btn btn-secondary text-dark btn-xs"><span
                             class="mr-2"><span class="fas fa-sign-out-alt"></span></span>Sign Out</a>
                 </div>
-                <form id="logout-form" action="{{ route('login') }}" method="POST" style="display: none;">
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                     @csrf
                     @method('HEAD')
                 </form>
@@ -26,7 +31,7 @@
         </div>
         <ul class="nav flex-column">
 
-            <!-- Sidebar Admin -->
+            <!-- Sidebar Dashboard -->
             <li class="nav-item {{ Request::route()->getName() == 'admin.dashboard' ? 'active' : '' }}">
                 <a href="{{ route('admin.dashboard') }}" class="nav-link">
                     <span class="sidebar-icon"><span class="fas fa-chart-pie"></span></span>
@@ -34,36 +39,39 @@
                 </a>
             </li>
 
-            <!-- Sidebar Event -->
-            <li
-                class="nav-item {{ (request()->is('admin/event*')) || (request()->is('admin/kategori*')) || (request()->is('admin/speaker*')) ? 'active' : '' }}">
-                <span class="nav-link  collapsed  d-flex justify-content-between align-items-center"
-                    data-toggle="collapse" data-target="#submenu-app">
-                    <span>
-                        <span class="sidebar-icon"><span class="fas fa-code"></span></span>
-                        Event
-                    </span>
-                    <span class="link-arrow"><span class="fas fa-chevron-right"></span></span>
-                </span>
-                <div class="multi-level collapse {{ (request()->is('admin/event*')) || (request()->is('admin/kategori*')) || (request()->is('admin/speaker*')) ? 'show' : '' }}"
-                    role="list" id="submenu-app" aria-expanded="false">
-                    <ul class="flex-column nav">
-                        <li class="nav-item {{  (request()->is('admin/kategori*')) ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('admin.kategori.index') }}"><span>Kategori
-                                    Event</span></a>
-                        <li class="nav-item {{ (request()->is('admin/speaker*')) ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('admin.speaker.index') }}"><span>Speaker Event</span></a>
-                        </li>
-                        <li class="nav-item {{ (request()->is('admin/event*')) ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('admin.event.index') }}"><span>Data Event</span></a>
-                        </li>
+            <!-- Sidebar Kategori -->
+            <li class="nav-item {{ (request()->is('admin/kategori*')) ? 'active' : '' }}">
+                <a href="{{ route('admin.kategori.index') }}" class="nav-link">
+                    <span class="sidebar-icon"><span class="fas fa-tag"></span></span>
+                    <span>Kategori Event</span>
+                </a>
             </li>
+
+            <!-- Sidebar Speaker -->
+            <li class="nav-item {{ (request()->is('admin/speaker*')) ? 'active' : '' }}">
+                <a href="{{ route('admin.speaker.index') }}" class="nav-link">
+                    <span class="sidebar-icon"><span class="fas fa-users"></span></span>
+                    <span>Speaker Event</span>
+                </a>
+            </li>
+
+            <!-- Sidebar Event -->
+            <li class="nav-item {{ (request()->is('admin/event*')) ? 'active' : '' }}">
+                <a href="{{ route('admin.event.index') }}" class="nav-link">
+                    <span class="sidebar-icon"><span class="fas fa-folder"></span></span>
+                    <span>Data Event</span>
+                </a>
+            </li>
+
+            <!-- Sidebar Event Registrasi -->
+            <li class="nav-item {{ (request()->is('admin/event-register*')) ? 'active' : '' }}">
+                <a href="{{ route('admin.event-register.index') }}" class="nav-link">
+                    <span class="sidebar-icon"><span class="fas fa-user-plus"></span></span>
+                    <span>Registrasi Event</span>
+                </a>
+            </li>
+
+
         </ul>
-    </div>
-    </li>
-
-
-
-    </ul>
     </div>
 </nav>
