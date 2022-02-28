6506a09aa3522237bd87c5a176094e93b6819fe0
hafet17 <do.crazy192@gmail.com>
2022-02-28 09:49:32 +0700

    feat(style): added link rel icon

--- resources/views/admin/layouts/style.blade.php
+++ resources/views/admin/layouts/style.blade.php
@@ -1,22 +1,22 @@
-    <!-- Fontawesome -->
-    <link type="text/css" href="{{ asset('assets/backend/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}"
-        rel="stylesheet">
+<link rel="icon" type="image/png" href="{{ asset('assets/frontend/img/logo-spillmoment.png') }}">
+<!-- Fontawesome -->
+<link type="text/css" href="{{ asset('assets/backend/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}"
+    rel="stylesheet">
 
-    <!-- Notyf -->
-    <link type="text/css" href="{{ asset('assets/backend/vendor/notyf/notyf.min.css') }}" rel="stylesheet">
+<!-- Notyf -->
+<link type="text/css" href="{{ asset('assets/backend/vendor/notyf/notyf.min.css') }}" rel="stylesheet">
 
-    <!-- Volt CSS -->
-    <link type="text/css" href="{{ asset('assets/backend/css/volt.css') }}" rel="stylesheet">
+<!-- Volt CSS -->
+<link type="text/css" href="{{ asset('assets/backend/css/volt.css') }}" rel="stylesheet">
 
-    <!-- DataTable -->
-    <link rel="stylesheet" href="{{ asset('assets/backend/vendor/datatables/css/dataTables.bootstrap5.min.css') }}">
+<!-- DataTable -->
+<link rel="stylesheet" href="{{ asset('assets/backend/vendor/datatables/css/dataTables.bootstrap5.min.css') }}">
 
-    <link rel="stylesheet" type="text/css"
-        href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
 
-    <style>
-        .ck-editor__editable {
-            min-height: 170px;
-        }
+<style>
+    .ck-editor__editable {
+        min-height: 170px;
+    }
 
-    </style>
+</style>
