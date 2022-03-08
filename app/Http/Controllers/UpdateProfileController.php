8011533fb4ec4a84839816a9296cd54274d4b0b9
hafet17 <do.crazy192@gmail.com>
2022-03-09 06:31:41 +0700

    feat(profile): implement query in controller

--- app/Http/Controllers/UpdateProfileController.php
+++ app/Http/Controllers/UpdateProfileController.php
@@ -6,61 +6,79 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use App\Rules\UserOldPassword;
+use Illuminate\Support\Facades\File;
 
 class UpdateProfileController extends Controller
 {
+
+	public function dashboard()
+	{
+		return view('auth.profile-dashboard');
+	}
+
 	public function changePasswordIndex()
 	{
-		 return view('auth.profile-change-password');
-	} 
+		return view('auth.profile-dashboard');
+	}
 
 	public function changePasswordStore(Request $request)
 	{
-		$user = User::findOrFail(Auth::id());
-
 		$request->validate([
-			'current_password' => ['required'],
-			'password' => ['required', 'confirmed', Rules\Password::defaults()],
-	  ]);
-		
-		if (Hash::check($request->current_password, $user->password)) { 
+			'current_password' => ['required', new UserOldPassword],
+			'password' => ['required', 'min:3'],
+			'password_confirmation' => ['same:password'],
+		]);
 
-			if (Hash::check($request->password, $user->password)) {
-				return redirect()->route('profile.password')->withErrors('Your password is the same as before !');
-			}
-
-			$user->fill([
-				'password' => Hash::make($request->password)
-			 ])->save();
-			 
-			return redirect()->route('profile.password')->with('status', 'Password changed !');
-		} else {
-			return redirect()->route('profile.password')->withErrors(['Your current password is wrong !']);
-		}
-
+		$user = User::findOrFail(Auth::id());
+		$user->update(['password' => Hash::make($request->password)]);;
+		return back()->with(['success' => 'Account berhasil diupdate!']);
 	}
 
 	public function changeDataIndex()
 	{
 		$user = User::findOrFail(Auth::id());
-		 return view('auth.profile-change-data', compact('user'));
-	} 
-	
+		return view('auth.profile-dashboard', compact('user'));
+	}
+
 	public function changeDataUpdate(Request $request)
 	{
 		$request->validate([
-				'name' => 'required|string|max:255',
-				'phone' => 'required|min:12|max:15|unique:users,phone,'.Auth::id(),
-				'gender' => 'in:pria,wanita',
+			'name' => 'required|string|max:255',
+			'email' => 'required|min:12|max:15|unique:users,email,' . Auth::id(),
+			'phone' => 'required|min:12|max:15|unique:users,phone,' . Auth::id(),
+			'gender' => 'required|in:pria,wanita',
+			// 'photo'	 => 'sometimes|nullable|image|mimes:jpeg,jpg,png,bmp',
+			'address' => 'required'
 		]);
 
-		User::findOrFail(Auth::id())->update([
+		/* if ($request->hasFile('photo')) {
+			File::delete('uploads/users/' . Auth::user()->photo);
+			$request->photo =  $request->file('photo');
+			$extention = $request->photo->getClientOriginalExtension();
+			$filename = time() . '.' . $extention;
+			$request->photo->move('uploads/users/', $filename);
+
+			User::findOrFail(Auth::id())->update([
 				'name' => $request->name,
+				'email' => $request->email,
 				'phone' => $request->phone,
 				'gender' => $request->gender,
+				'photo' => $filename,
 				'address' => $request->address,
+			]); */
+		// } else {
+		// }
+
+
+		User::findOrFail(Auth::id())->update([
+			'name' => $request->name,
+			'email' => $request->email,
+			'phone' => $request->phone,
+			'gender' => $request->gender,
+			'address' => $request->address,
 		]);
-		 return back()->with('status', 'Update success.');
-	} 
+
+		return back()->with(['success' => 'Profile berhasil diupdate!']);
+	}
 }
