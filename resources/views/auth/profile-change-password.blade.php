40017df8d8709174849141efca66989ae0a66b66
hafet17 <do.crazy192@gmail.com>
2022-03-09 06:32:45 +0700

    feat(profile): re-design views change password

--- resources/views/auth/profile-change-password.blade.php
+++ resources/views/auth/profile-change-password.blade.php
@@ -1,67 +1,60 @@
-@extends('layouts.app')
-
-@section('content')
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Change Password with Current Password Validation</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('profile.password.store') }}">
-                        @csrf
-								@method('put')  
-
-								 <!-- Session Status -->
-								@if (Session::has('status'))
-									<div class="alert alert-success">
-										<ul>
-												<li>{{ Session::get('status') }}</li>
-										</ul>
-									</div>
-								@endif
-
-								<!-- Validation Errors -->
-								@if($errors->any())
-									<h4 class="text-danger">{{$errors->first()}}</h4>
-								@endif
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Current Password</label>
-                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="current_password" autocomplete="current-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">New Password</label>
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" />
-                            </div>
-                        </div> 
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">New Confirm Password</label>  
-                            <div class="col-md-6">
-                                <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" />
-                            </div>
-                        </div>  
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Update Password
-                                </button>
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
+<div class="card">
+    <div class="card-header mb-2 text-dark bg-profile">
+        <div class="clearfix">
+            <h5 class="card-title mb-4">Pengaturan Account</h4>
         </div>
     </div>
+    <div class="card-body">
+        <form class="row g-3 needs-validation" method="POST" action="{{ route('profile.password.store') }}">
+            @csrf
+            @method('put')
 
+            @if (session('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <i class="fas fa-check-circle    "></i>
+                <strong>{{ session('success') }}</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+            @endif
+
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="validationCustom01" class="form-label">Password Sebelumnya</label>
+                    <input type="password"
+                        class="form-control {{ $errors->first('current_password') ? 'is-invalid' : '' }}"
+                        name="current_password" id="validationCustom01" placeholder="Masukkan Password Sebelumnya">
+                    <div class="invalid-feedback">
+                        {{$errors->first('current_password')}}
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="validationCustom02" class="form-label">Password Baru</label>
+                    <input type="password" class="form-control 
+                    {{ $errors->first('password') ? 'is-invalid' : '' }}" id="validationCustom02" name="password"
+                        placeholder="Masukkan Password Baru">
+                    <div class="invalid-feedback">
+                        {{$errors->first('password')}}
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="validationCustom03" class="form-label">Konfirmasi Password Baru</label>
+                    <input type="password"
+                        class="form-control {{ $errors->first('password_confirmation') ? 'is-invalid' : '' }}"
+                        name="password_confirmation" id="validationCustom03"
+                        placeholder="Masukkan Konfirmasi Password Baru">
+                    <div class="invalid-feedback">
+                        {{$errors->first('password_confirmation')}}
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <button class="btn btn-profile" type="submit">Update Account</button>
+            </div>
+        </form>
+    </div>
 </div>
-
-@endsection
