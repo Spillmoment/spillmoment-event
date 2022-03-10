5f59069f68538df09de85f7f09c9c891ddde8ca0
hafet17 <do.crazy192@gmail.com>
2022-03-10 08:35:28 +0700

    feat(register): added attribut autofocus in views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,48 +22,53 @@
                             <div class="auth-body">
                                 <p>Masukan Nama</p>
                                 <div class="auth-input-box">
-                                    <input type="text" name="name" id="nama" placeholder="nama kamu?"  value="{{ old('name') }}" />
-											</div>
-											@error('name')
-										  <small style="color: rgb(255, 78, 78); font-weight: bold">{{ $message }}</small>
+                                    <input type="text" name="name" id="nama" placeholder="nama kamu?" autofocus
+                                        value="{{ old('name') }}" />
+                                </div>
+                                @error('name')
+                                <small style="color: rgb(255, 78, 78); font-weight: bold">{{ $message }}</small>
                                 @enderror
 
                                 <br />
                                 <p>Alamat Email</p>
                                 <div class="auth-input-box">
-                                    <input type="email" name="email" id="email" placeholder="Masukan alamat email yang ada" value="{{ old('email') }}" />
+                                    <input type="email" name="email" id="email"
+                                        placeholder="Masukan alamat email yang ada" value="{{ old('email') }}" />
                                 </div>
-										  @error('email')
-										  <small style="color: rgb(255, 78, 78); font-weight: bold">{{ $message }}</small>
+                                @error('email')
+                                <small style="color: rgb(255, 78, 78); font-weight: bold">{{ $message }}</small>
                                 @enderror
 
                                 <br />
                                 <p>Nomor Ponsel (opsional)</p>
                                 <div class="auth-input-box">
-                                    <input type="number" name="phone" id="phone" placeholder="contoh: +6208974xxxx" value="{{ old('phone') }}" />
+                                    <input type="number" name="phone" id="phone" placeholder="contoh: +6208974xxxx"
+                                        value="{{ old('phone') }}" />
                                 </div>
-										  @error('phone')
-										  <small style="color: rgb(255, 78, 78); font-weight: bold">{{ $message }}</small>
+                                @error('phone')
+                                <small style="color: rgb(255, 78, 78); font-weight: bold">{{ $message }}</small>
                                 @enderror
-                                
-										  <br />
+
+                                <br />
                                 <p>Jenis kelamin</p>
                                 <div class="auth-input-box">
                                     <div class="flex-input">
                                         <select name="gender" id="select-input gender">
-                                            <option value="pria" {{old('gender') == 'pria' ? 'selected' : ''}}>Pria</option>
-                                            <option value="wanita" {{old('gender') == 'wanita' ? 'selected' : ''}}>Wanita</option>
+                                            <option value="pria" {{old('gender') == 'pria' ? 'selected' : ''}}>Pria
+                                            </option>
+                                            <option value="wanita" {{old('gender') == 'wanita' ? 'selected' : ''}}>
+                                                Wanita</option>
                                         </select>
                                         <button>
                                             <fa :icon="['fas', 'caret-down']" />
                                         </button>
                                     </div>
                                 </div>
-										  @error('gender')
-										  <small style="color: rgb(255, 78, 78); font-weight: bold">{{ $message }}</small>
+                                @error('gender')
+                                <small style="color: rgb(255, 78, 78); font-weight: bold">{{ $message }}</small>
                                 @enderror
-                                
-										  <br />
+
+                                <br />
                                 <p>Password</p>
                                 <div class="auth-input-box">
                                     <div class="input-box-password">
@@ -82,35 +87,35 @@
                                 </div>
                                 <small id="password-text"></small>
 
-										  <br />
-											<p>Konfirmasi Password</p>
-											<div class="auth-input-box">
-												<div class="input-box-password">
-														<input type="password" name="password_confirmation" placeholder=""
-															v-on:keyup="isGood(this.value)" id="password_confirmation" />
-														<label for="password-function" class="checkbox-pass">
-															<input type="checkbox" name="" id="password-function" />
-															<div class="icon-eye1 icon">
-																<fa :icon="['fas', 'eye-slash']" />
-															</div>
-															<div class="icon-eye2 icon">
-																<fa :icon="['fas', 'eye']" />
-															</div>
-														</label>
-												</div>
-											</div>
-											<small id="password-text"></small>
-											@error('password')
-										  <small style="color: rgb(255, 78, 78); font-weight: bold">{{ $message }}</small>
+                                <br />
+                                <p>Konfirmasi Password</p>
+                                <div class="auth-input-box">
+                                    <div class="input-box-password">
+                                        <input type="password" name="password_confirmation" placeholder=""
+                                            v-on:keyup="isGood(this.value)" id="password_confirmation" />
+                                        <label for="password-function" class="checkbox-pass">
+                                            <input type="checkbox" name="" id="password-function" />
+                                            <div class="icon-eye1 icon">
+                                                <fa :icon="['fas', 'eye-slash']" />
+                                            </div>
+                                            <div class="icon-eye2 icon">
+                                                <fa :icon="['fas', 'eye']" />
+                                            </div>
+                                        </label>
+                                    </div>
+                                </div>
+                                <small id="password-text"></small>
+                                @error('password')
+                                <small style="color: rgb(255, 78, 78); font-weight: bold">{{ $message }}</small>
                                 @enderror
-									</div>
+                            </div>
 
-                        <div class="wrap-button">
-                            <br />
-                            <button @click="reg">Lanjutkan</button>
-                            <br /><br />
-                        </div>
-								</form>
+                            <div class="wrap-button">
+                                <br />
+                                <button @click="reg">Lanjutkan</button>
+                                <br /><br />
+                            </div>
+                        </form>
                         <p id="hint">
                             Sudah punya akun terdaftar? <a href={{ route('login') }}><b>Masuk!</b></a>
                         </p>
