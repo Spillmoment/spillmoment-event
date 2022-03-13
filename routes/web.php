40e73284d1e2833684152eba0bb5e541ed213da1
Deddy Gunawan <deddygunawan98@gmail.com>
2022-03-13 23:57:58 +0700

    add route partner getAutoComplete

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::prefix('event')
 		Route::get('', [EventController::class, 'index'])->name('index');
 		Route::get('filter', [EventController::class, 'filter'])->name('filter');
 		Route::get('kategori-event', [EventController::class, 'kategoriGetAutocomplete'])->name('kategori.getAutocomplte');
+		Route::get('partner-event', [EventController::class, 'partnerGetAutocomplete'])->name('partner.getAutocomplte');
 		Route::get('detail/{slug}', [EventController::class, 'detail'])->name('detail');
 		Route::post('join-event/{event_id}', [EventController::class, 'join'])->name('join');
 	});
