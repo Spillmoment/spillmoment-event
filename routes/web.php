bab408da749ceaf275349f3eb836d93e67c89db3
hafet17 <do.crazy192@gmail.com>
2022-03-25 10:13:36 +0700

    refactor: route web

--- routes/web.php
+++ routes/web.php
@@ -2,58 +2,92 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Admin\{DashboardControlller, EventRegisterControlller, KategoriEventController, PartnerController, SpeakerController};
-use App\Http\Controllers\Admin\EventController as AdminEventController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\UpdateProfileController;
+use App\Http\Controllers\Admin\{
+	DashboardControlller,
+	EventController as AdminEventController,
+	EventRegisterControlller,
+	KategoriEventController,
+	PartnerController,
+	SpeakerController
+};
+use App\Http\Controllers\Web\{
+	HomeController,
+	EventController,
+	ProfileController
+};
 
 // User auth
 require __DIR__ . '/auth.php';
 
-Route::get('/', function () {
-	return view('home');
-});
+Route::get('', HomeController::class)
+	->name('home');
+Route::get('dashboard', [ProfileController::class, 'dashboard'])
+	->middleware(['auth'])
+	->name('dashboard');
 
 Route::prefix('event')
 	->name('event.')
 	->group(function () {
-		Route::get('', [EventController::class, 'index'])->name('index');
-		Route::get('filter', [EventController::class, 'filter'])->name('filter');
-		Route::get('kategori-event', [EventController::class, 'kategoriGetAutocomplete'])->name('kategori.getAutocomplte');
-		Route::get('partner-event', [EventController::class, 'partnerGetAutocomplete'])->name('partner.getAutocomplte');
-		Route::get('detail/{slug}', [EventController::class, 'detail'])->name('detail');
-		Route::post('join-event/{event_id}', [EventController::class, 'join'])->name('join');
+		Route::get('', [EventController::class, 'index'])
+			->name('index');
+		Route::get('filter', [EventController::class, 'filter'])
+			->name('filter');
+		Route::get('kategori-event', [EventController::class, 'kategoriGetAutocomplete'])
+			->name('kategori.getAutocomplte');
+		Route::get('partner-event', [EventController::class, 'partnerGetAutocomplete'])
+			->name('partner.getAutocomplte');
+		Route::get('detail/{slug}', [EventController::class, 'detail'])
+			->name('detail');
+		Route::post('join-event/{event_id}', [EventController::class, 'join'])
+			->name('join');
 	});
 
-Route::get('/dashboard', [UpdateProfileController::class, 'dashboard'])->middleware(['auth'])->name('dashboard');
 
 // Admin All Feature
-Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+Route::namespace('Admin')
+	->prefix('admin')
+	->name('admin.')->group(function () {
 
-	Route::namespace('Auth')->middleware('guest:admin')->group(function () {
-		// Login
-		Route::get('login', 'AuthenticatedSessionController@create')->name('login');
-		Route::post('login', 'AuthenticatedSessionController@store')->name('adminlogin');
+		// Authenticate
+		Route::namespace('Auth')->middleware('guest:admin')
+			->group(function () {
+				// Login
+				Route::get('login', 'AuthenticatedSessionController@create')
+					->name('login');
+				Route::post('login', 'AuthenticatedSessionController@store')
+					->name('adminlogin');
 
-		Route::get('forgot-password', 'PasswordResetLinkController@create')->name('password.request');
-		Route::post('forgot-password', 'PasswordResetLinkController@store')->name('password.email');
-		Route::get('reset-password/{token}', 'NewPasswordController@create')->name('password.reset');
-		Route::post('reset-password', 'NewPasswordController@store')->name('password.update');
+				Route::get('forgot-password', 'PasswordResetLinkController@create')
+					->name('password.request');
+				Route::post('forgot-password', 'PasswordResetLinkController@store')
+					->name('password.email');
+				Route::get('reset-password/{token}', 'NewPasswordController@create')
+					->name('password.reset');
+				Route::post('reset-password', 'NewPasswordController@store')
+					->name('password.update');
+			});
+
+		// Admin
+		Route::middleware('admin')
+			->group(function () {
+				Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+					->name('logout');
+				// Dashboard
+				Route::get('dashboard', [DashboardControlller::class, 'index'])
+					->name('dashboard');
+				// Event
+				Route::resource('kategori', KategoriEventController::class)
+					->except(['show']);
+				Route::resource('speaker', SpeakerController::class);
+				Route::resource('partner', PartnerController::class)
+					->except(['show']);
+				Route::resource('event', AdminEventController::class);
+				// Register Event
+				Route::get('register-event', [EventRegisterControlller::class, 'index'])
+					->name('register-event.index');
+				Route::get('register-event/{id}', [EventRegisterControlller::class, 'show'])
+					->name('register-event.show');
+				Route::put('confirm-event/{id}/{state}', [EventRegisterControlller::class, 'confirm'])
+					->name('confirm-event');
+			});
 	});
-
-	Route::middleware('admin')->group(function () {
-		Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
-		// Route::get('dashboard', 'HomeController@index')->name('dashboard');
-		// Dashboard
-		Route::get('dashboard', [DashboardControlller::class, 'index'])->name('dashboard');
-		// Event
-		Route::resource('kategori', KategoriEventController::class)->except(['show']);
-		Route::resource('speaker', SpeakerController::class);
-		Route::resource('partner', PartnerController::class)->except(['show']);
-		Route::resource('event', AdminEventController::class);
-		// Register Event
-		Route::get('register-event', [EventRegisterControlller::class, 'index'])->name('register-event.index');
-		Route::get('register-event/{id}', [EventRegisterControlller::class, 'show'])->name('register-event.show');
-		Route::put('confirm-event/{id}/{state}', [EventRegisterControlller::class, 'confirm'])->name('confirm-event');
-	});
-});
