a7a3f1ab54a64097cb5e56c50088720a236cf5ba
hafet17 <do.crazy192@gmail.com>
2022-03-25 22:00:38 +0700

    feat(vendor): create fillable & relations

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Vendor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'nik',
+        'name',
+        'slug',
+        'description',
+        'photo',
+        'whatsapp',
+        'instagram',
+        'address',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
