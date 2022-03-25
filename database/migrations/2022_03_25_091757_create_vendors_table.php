0ee01c1872c9e0777162d5b3df5f2854fd4323f6
hafet17 <do.crazy192@gmail.com>
2022-03-25 09:24:19 +0700

    add: models & migration vendors

---
+++ database/migrations/2022_03_25_091757_create_vendors_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('nik', 16)->nullable();
+            $table->string('name', 255);
+            $table->string('slug', 255);
+            $table->longText('description');
+            $table->string('photo', 255);
+            $table->string('whatsapp', 15);
+            $table->string('instagram', 100);
+            $table->longText('address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
