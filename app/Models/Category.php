7da92dfe482ed0db0a5811a7c83b2ba95687efaa
hafet17 <do.crazy192@gmail.com>
2022-03-25 22:00:14 +0700

    feat(category): create fillable & relations

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'image',
+    ];
+
+    public function getCreatedAtAttribute()
+    {
+        return Carbon::parse($this->attributes['created_at'])
+            ->translatedFormat('d F Y');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
