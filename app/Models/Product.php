cea1abb9db9969f477a02d7a5bbf275fd17e1cae
hafet17 <do.crazy192@gmail.com>
2022-03-25 22:00:27 +0700

    feat(product): create fillable & relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'vendor_id', 'category_id', 'code', 'name', 'slug', 'description', 'photos', 'price', 'discount', 'stock', 'status'
+    ];
+
+    public function getCreatedAtAttribute()
+    {
+        return Carbon::parse($this->attributes['created_at'])
+            ->translatedFormat('d F Y');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
 }
