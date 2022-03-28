942d6745be34c2bdf47136487c560613947f68a5
hafet17 <do.crazy192@gmail.com>
2022-03-28 09:44:17 +0700

    fix: relations table products

--- database/migrations/2022_03_25_091807_create_products_table.php
+++ database/migrations/2022_03_25_091807_create_products_table.php
@@ -15,16 +15,28 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained();
-            $table->foreignId('vendor_id')->constrained();
+
+            $table->foreignId('category_id');
+            $table->foreign('event_category_id')->references('id')
+                ->on('categories')->onDelete('cascade');
+
+            $table->foreignId('vendor_id');
+            $table->foreign('vendor_id')->references('id')
+                ->on('vendors')->onDelete('cascade');
+
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')
+                ->on('users')->onDelete('cascade');
+
             $table->string('code', 50)->unique();
             $table->string('name', 100);
             $table->string('slug')->unique();
             $table->longText('description')->nullable();
             $table->string('photos')->nullable();
-            $table->integer('price');
-            $table->integer('discount');
+            $table->integer('price')->default(0);
+            $table->integer('discount')->default(0);
             $table->integer('stock');
+            $table->string('link');
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
