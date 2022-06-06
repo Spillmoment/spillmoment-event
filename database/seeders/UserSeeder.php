8ef5d552b358d91689e4e8667689d907dc42a320
hafet17 <do.crazy192@gmail.com>
2022-06-06 13:03:46 +0700

    feat: completed user seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -15,13 +16,37 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $user = [
-            'name' => 'User Testing',
-            'email' => 'user@test.com',
-            'phone' => '085236639887',
-            'gender' => 'pria',
-            'password' => bcrypt('akudewe')
-        ];
-        User::create($user);
+
+        DB::table('users')->insert([
+            [
+                'name' => 'User Testing',
+                'email' => 'user@test.com',
+                'phone' => '085236639887',
+                'gender' => 'pria',
+                'password' => bcrypt('akudewe')
+            ],
+            [
+                'name' => 'Uchiha Madara',
+                'email' => 'madara@test.com',
+                'phone' => '085236639888',
+                'gender' => 'pria',
+                'password' => bcrypt('akudewe')
+            ],
+            [
+                'name' => 'Gojo Satorou',
+                'email' => 'gojo@test.com',
+                'phone' => '085236639187',
+                'gender' => 'pria',
+                'password' => bcrypt('akudewe')
+            ],
+            [
+                'name' => 'Deddy Sujar',
+                'email' => 'deddy@test.com',
+                'phone' => '085216639887',
+                'gender' => 'pria',
+                'password' => bcrypt('akudewe')
+            ],
+
+        ]);
     }
 }
