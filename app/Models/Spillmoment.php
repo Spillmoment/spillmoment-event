9919c98e581e1d7d4ecb1e6151e6ad09f865b76b
hafet17 <do.crazy192@gmail.com>
2022-06-06 13:02:58 +0700

    add(models): vendor belongs relationship

--- app/Models/Spillmoment.php
+++ app/Models/Spillmoment.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Spillmoment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'vendor_id', 'title', 'slug', 'description', 'image', 'link'
+    ];
+
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
 }
