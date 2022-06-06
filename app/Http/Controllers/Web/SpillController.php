4b7604acb1cce9e8553c37785705964ca690acc0
hafet17 <do.crazy192@gmail.com>
2022-06-06 13:04:58 +0700

    add: spillmoment controller

--- app/Http/Controllers/Web/SpillController.php
+++ app/Http/Controllers/Web/SpillController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Spillmoment;
 
 class SpillController extends Controller
 {
-    //
+    public function index()
+    {
+        $data['spill'] = Spillmoment::latest()->get();
+        return view('web.home.index', $data);
+    }
 }
